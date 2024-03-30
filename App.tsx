753b0b4158e3c980ca9d2e4e4b8cf140b64962ac
Gene Tigner <gene@Genes-MBP.lan>
2024-03-29 19:17:11 -0500

    updated the app.tsx file

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import {
+  Image,
   SafeAreaView,
   StatusBar,
   StyleSheet,
@@ -15,11 +16,14 @@ import {
 } from 'react-native';
 
 function App(): React.JSX.Element {
+  const [bookLink, setBookLink] = React.useState({uri: "https://covers.openlibrary.org/b/isbn/9780385533225-M.jpg"});
+  console.log(bookLink);
   return (
     <SafeAreaView style={styles.safeArea}>
       <StatusBar
       />
       <View style={styles.container}>
+        <Image style={styles.imageContainer} source={bookLink} resizeMode="contain"></Image>
         <Text style={styles.textContainer}>Hello World! 👋</Text>
       </View>
     </SafeAreaView>
@@ -35,8 +39,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  imageContainer: {
+    alignSelf: 'center',
+    height: "50%",
+    width: "60%",
+    backgroundColor: "red"
+  },
   textContainer: {
     textAlign: 'center',
+    fontSize: 42,
+    fontWeight: "bold"
   },
 });
 
